c3db46a3c0c60e29061790284cb4f68d4133018a
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-09-03 19:48:41 -0700

    Add functions to the buttons

---
+++ app/src/main/java/com/example/sowmya/myrestaurants/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.sowmya.myrestaurants;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+}
